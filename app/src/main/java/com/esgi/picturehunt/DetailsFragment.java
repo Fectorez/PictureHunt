ee57f7ad90340456c24ec05ad3709edbbd7419d7
unknown <maxence.cadiot@hotmail.fr>
2019-02-21 22:40:29 +0100

    DetailsFragment

--- app/src/main/java/com/esgi/picturehunt/DetailsFragment.java
+++ app/src/main/java/com/esgi/picturehunt/DetailsFragment.java
@@ -7,12 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailsFragment extends Fragment {
     public static final String REFERENCE_PHOTOS_TO_HUNT = "photosToHunt";
+    private TextView user;
+    private ImageView image;
+    private Button play;
 
     public DetailsFragment() {
     }
@@ -28,5 +36,12 @@ public class DetailsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Bundle bundle = getArguments();
         PhotoToHunt photoToHunt = (PhotoToHunt)bundle.getSerializable(REFERENCE_PHOTOS_TO_HUNT);
+
+        user = view.findViewById(R.id.user);
+        image = view.findViewById(R.id.image);
+        play = view.findViewById(R.id.play);
+
+        user.setText(photoToHunt.getUserId());
+        Picasso.get().load(photoToHunt.getImage()).resize(300, 200).into(image);
     }
 }
